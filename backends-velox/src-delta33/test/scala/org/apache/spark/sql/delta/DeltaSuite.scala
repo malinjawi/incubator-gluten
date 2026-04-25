6db4a2ce2c2dfcd2739bc7010f67dc80ce1f6ed4
Mohammad Linjawi <Mohammad.Linjawi@ibm.com>
2026-04-25 16:44:18 +0300

    [VL][Delta] Restore SQLConf import in DeltaSuite

--- backends-velox/src-delta33/test/scala/org/apache/spark/sql/delta/DeltaSuite.scala
+++ backends-velox/src-delta33/test/scala/org/apache/spark/sql/delta/DeltaSuite.scala
@@ -34,6 +34,7 @@ import org.apache.spark.sql.delta.util.FileNames
 import org.apache.spark.sql.execution.datasources.{HadoopFsRelation, LogicalRelationWithTable}
 import org.apache.spark.sql.execution.streaming.MemoryStream
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.streaming.StreamingQuery
 import org.apache.spark.sql.test.SharedSparkSession
 import org.apache.spark.sql.types.StructType
